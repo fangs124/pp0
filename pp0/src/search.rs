ab07cd9cc3340050c5ec8d3a94254ccdc5e5e11c
Fangs <fangs124@users.noreply.github.com>
2025-11-06 18:14:20 +0700

    Heimdall-tech to make it less neanderthal

--- pp0/src/search.rs
+++ pp0/src/search.rs
@@ -345,7 +345,7 @@ impl SearchData {
         &mut self, chessgame: &mut ChessGame, a: i16, b: i16, d: usize, ev: &mut impl Evaluator, tt: Arc<TT>, time_limit: Option<(Instant, Duration)>,
         node_limit: Option<NonZero<usize>>,
     ) -> i16 {
-        let (chessmoves, gamestate) = chessgame.try_generate_moves();
+        let (mut chessmoves, gamestate) = chessgame.try_generate_moves();
 
         if let GameState::Finished(state) = gamestate {
             match state {
@@ -355,6 +355,10 @@ impl SearchData {
                 GameResult::Draw => return 0,
             }
         }
+
+        let mut alpha: i16 = a;
+        let mut best_score: i16 = i16::MIN + 1;
+        let mut best_move: Option<ChessMove> = None;
 
         if let Some(position_data) = tt.load(chessgame.hash(), Ordering::Relaxed) {
             if position_data.depth() as usize >= d {
@@ -371,6 +375,17 @@ impl SearchData {
                     _ => (),
                 }
             }
+
+            if let Some(chessmove) = position_data.best() {
+                let mut i: usize = 0;
+                while i < chessmoves.len() {
+                    if chessmove == chessmoves[i] {
+                        chessmoves[i] = chessmoves[0];
+                        chessmoves[0] = chessmove;
+                    }
+                    i += 1;
+                }
+            }
         }
 
         let d = match chessgame.is_in_check() {
@@ -381,9 +396,7 @@ impl SearchData {
         if d == 0 {
             return ev.eval(&chessgame);
         }
-        let mut alpha: i16 = a;
-        let mut best_score: i16 = i16::MIN + 1;
-        let mut best_move: Option<ChessMove> = None;
+
         //let mut best_move: Option<ChessMove> = None;
         for chessmove in chessmoves {
             //chef: only check every 1024 node
