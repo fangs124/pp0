61b309f155f9844cf49bff45976db70a51e3a49d
Fangs <fangs124@users.noreply.github.com>
2025-11-08 09:19:30 +0700

    naive first implementation - bench: 306147

--- chessbb/src/chessboard/movegen.rs
+++ chessbb/src/chessboard/movegen.rs
@@ -50,6 +50,7 @@ impl ChessBoard {
             _ => self.king_moves::<true>(&mut moves),
         }
 
+        self.sort_moves(&mut moves); //naive implementation, insertion sort is probably better
         return moves;
     }
 
