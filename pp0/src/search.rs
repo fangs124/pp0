9b44c5e8aef35738454ea8c531e56c5d535e44a8
Fangs <fangs124@users.noreply.github.com>
2025-11-12 12:37:04 +0700

    bench: 381715

--- pp0/src/search.rs
+++ pp0/src/search.rs
@@ -379,10 +379,8 @@ impl SearchData {
         let mut best_score: i16 = i16::MIN + 1;
         let mut best_move: Option<ChessMove> = None;
 
-        if let Some(position_data) = tt.load(chessgame.hash(), Ordering::Relaxed)
-            && self.ply > 1
-        {
-            if position_data.depth() as usize >= d {
+        if let Some(position_data) = tt.load(chessgame.hash(), Ordering::Relaxed) {
+            if position_data.depth() as usize >= d && self.ply > 1 {
                 match position_data.ty() {
                     NodeType::Exact => {
                         return position_data.eval();
